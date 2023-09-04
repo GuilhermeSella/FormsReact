417ba5027d9938707c1e1c5d58afc60f9979e237
Guilherme Sella <guilhermesella1974@gmail.com>
2023-09-03 23:23:23 -0300

    Schema validations com Zod

--- src/App.jsx
+++ src/App.jsx
@@ -8,7 +8,10 @@ import {zodResolver} from "@hookform/resolvers/zod"
 const schema = z.object({
   name: z.string().nonempty("O campo nome é obrigatório"),
   email: z.string().nonempty("Digite um email válido").email("O Campo email é obrigatório"),
-  senha: z.string().nonempty("O campo senha é obrigatório").min(3, "A senha deve ter pelo menos 6 caracteres").max(12, "A senha deve ter no máximo 12 caracteres")
+  senha: z.string().nonempty("O campo senha é obrigatório").min(3, "A senha deve ter pelo menos 6 caracteres").max(12, "A senha deve ter no máximo 12 caracteres"),
+  telefone: z.string().refine((value) => /^\d{2} ?\d{9}$/.test(value), {
+    message:"Digite um telefone válido no formato DD + 9 números."
+  } )
 })
 
 
@@ -27,7 +30,7 @@ function App() {
       <form className='w-screen h-screen flex flex-col items-center justify-center gap-5 ' onSubmit={handleSubmit(handleSave)}>
         <h1 className='text-4xl pb-10 font-semibold'>Formulário</h1>
 
-        <div className='flex flex-col items-start gap-2'>
+        <div className='flex flex-col items-start gap-2 '>
           <input 
           className='w-96 border border-slate-200  outline-none rounded py-3 px-4'
           type="text"
@@ -40,7 +43,7 @@ function App() {
 
          </div>
 
-        <div className='flex flex-col items-start gap-2'>
+        <div className='flex flex-col items-start gap-2 '>
           <input 
           className='w-96 border border-slate-200  outline-none rounded py-3 px-4'
           type="text" 
@@ -51,7 +54,17 @@ function App() {
 
         </div>
 
-        <div className='flex flex-col items-start gap-2'>
+        <div className='flex flex-col items-start gap-2 '>
+          <input 
+          className='w-96 border border-slate-200  outline-none rounded py-3 px-4'
+          type="text" 
+          placeholder='Digite o seu telefone...'
+          {...register("telefone")}
+          id="telefone" />
+             {errors.telefone && <p className='font-semibold text-red-600 text-left'>{errors.telefone.message}</p>}
+        </div>
+
+        <div className='flex flex-col items-start gap-2 '>
           <input 
           className='w-96 border border-slate-200  outline-none rounded py-3 px-4'
           type="text" 
@@ -61,7 +74,7 @@ function App() {
              {errors.senha && <p className='font-semibold text-red-600 text-left'>{errors.senha.message}</p>}
         </div>
 
-        <input type="submit" className=' w-96 py-2 px-3  bg-slate-700 font-semibold text-white rounded' value="Enviar" />
+        <input type="submit" className=' w-96 py-2 px-3 cursor-pointer  bg-slate-700 font-semibold text-white rounded' value="Enviar" />
       </form>
     </>
   )
